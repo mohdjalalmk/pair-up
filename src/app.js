9a4c87bd0834f7662716600a96e0fe102e055c20
Jalal <jalalmohammed1818@gmail.com>
2025-06-11 18:05:11 +0530

    wild card error for all errors

--- src/app.js
+++ src/app.js
@@ -6,8 +6,18 @@ const app = express()
 app.use("/admin",adminAuth)
 
 app.get("/user",userAuth,(req,res)=>{
+
     let userid=req.query.userid
     res.send(`user id is ${userid}`)
+    
+})
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        console.log(err);
+        
+        res.status(500).send("Something went wrong")
+    }
 })
 
 app.listen(3000,()=>{
